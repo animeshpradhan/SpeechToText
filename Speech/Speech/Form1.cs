574837364af1db96987e0c2b698805af5dc48af5
Animesh Pradhan <animeshpradhankanha123@gmail.com>
2020-04-26 09:52:33 +0530

    Add files via upload

---
+++ Speech/Speech/Form1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Speech;
+using System.Speech.Synthesis;
+namespace Speech
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        SpeechSynthesizer reader = new SpeechSynthesizer();
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if(richTextBox1.Text != "")
+            {
+                reader.Dispose();
+                reader = new SpeechSynthesizer();
+                reader.SpeakAsync(richTextBox1.Text);
+
+            }
+            else
+            {
+                MessageBox.Show("Enter some Text");
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if(reader!=null)
+            {
+                if(reader.State== SynthesizerState.Speaking)
+                {
+                    reader.Pause();
+                }
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (reader != null)
+            {
+                if (reader.State == SynthesizerState.Paused)
+                {
+                    reader.Resume();
+                }
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if(reader!=null)
+            {
+                reader.Dispose();
+            }
+        }
+    }
+}
